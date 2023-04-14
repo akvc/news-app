6d5fc0ef83806b053e1edb541e2564161cd015d6
Joanna Kuc <joa.kuc@protonmail.com>
2023-04-14 15:45:58 +0200

    Add browser router and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
-import './components/HomePage/HomePage';
+import './../node_modules/bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './components/HomePage/HomePage';
+import { Navbar } from './components/Navbar/Navbar';
+import { LoginPage } from './components/LoginPage/LoginPage';
+import { RegisterPage } from './components/RegisterPage/RegisterPage';
+import { UserPage } from './components/UserPage/UserPage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/user" element={<UserPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
