3924bf8806cb74cf6e34fde51c8d508256680ae3
Joanna Kuc <joa.kuc@protonmail.com>
2023-04-14 15:44:51 +0200

    Create simple navbar with logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,1 +1,42 @@
-export const Navbar = () => {};
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">PenguinNews</span>
+          {/* <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form> */}
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item dropdown">
+              <Link
+                to="/user"
+                className="nav-link dropdown-toggle"
+                role="button"
+              >
+                <img
+                  className="rounded-circle"
+                  width="40px"
+                  height="40px"
+                  alt="avatar"
+                  src="https://nftnow.com/wp-content/uploads/2022/08/pudgy-penguin-6873.png"
+                />
+              </Link>
+              <div className="dropdown-menu"></div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
