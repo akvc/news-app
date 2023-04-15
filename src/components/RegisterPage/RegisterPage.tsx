1be886a6009eeb162623b1e363f29df66ce76f94
Joanna Kuc <joa.kuc@protonmail.com>
2023-04-15 05:41:21 +0200

    Add react hook form

--- src/components/RegisterPage/RegisterPage.tsx
+++ src/components/RegisterPage/RegisterPage.tsx
@@ -1,3 +1,28 @@
+import { useForm } from 'react-hook-form';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
+
+interface RegisterFormData {
+  email: 'string';
+  password: 'string';
+  password2: 'string';
+}
+
 export const RegisterPage = () => {
-  return <div>Register Page</div>;
+  //const { register, handleSubmit } = useForm<RegisterFormData>;
+
+  const registerUser = (data: RegisterFormData) => {
+    if (data.password === data.password2) {
+      createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then((userCredentials) => {
+          console.log('User registered', userCredentials);
+        })
+        .catch((error) => {
+          console.log('Error');
+        });
+    } else {
+      alert('Passwords do not match');
+    }
+  };
+  return <div>Register</div>;
 };
