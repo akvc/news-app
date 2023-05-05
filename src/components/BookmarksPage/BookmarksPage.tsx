0cbea8508c5fe54f2d1f2d52c059521bf2820781
Joanna Kuc <joa.kuc@protonmail.com>
2023-05-05 12:40:06 +0200

    Add clear page button

--- src/components/BookmarksPage/BookmarksPage.tsx
+++ src/components/BookmarksPage/BookmarksPage.tsx
@@ -1,7 +1,7 @@
 import { Article } from '../Article/Article';
 import { ArticleObj } from '../HomePage/HomePage';
 import { auth, database } from '../../firebaseConfig';
-import { ref, onValue } from 'firebase/database';
+import { ref, onValue, remove } from 'firebase/database';
 import { useState, useEffect } from 'react';
 import './BookmarksPage.css';
 
@@ -29,11 +29,33 @@ export const BookmarksPage = ({ signedIn }: BookmarksPageProps) => {
     }
   }, [signedIn]);
 
+  const deleteBookmarkData = () => {
+    if (signedIn && auth.currentUser) {
+      //db reference
+      const dbRef = ref(
+        database,
+        'user/' + auth.currentUser.uid + '/bookmarks/'
+      );
+      remove(dbRef).then(() => {
+        console.log('Bookmarks removed');
+        setArticles([]);
+      });
+    }
+  };
+
   return (
     <>
       {signedIn && auth.currentUser && (
         <div>
-          <h1 className="m-4">Bookmarks page</h1>
+          <header className="mx-5">
+            <h1 className="m-4">Bookmarks page</h1>
+            <button
+              className="btn btn-primary my-2 p-2"
+              onClick={deleteBookmarkData}
+            >
+              Clear page
+            </button>
+          </header>
           <div className="articles-display mx-5 p-5">
             {articles !== undefined &&
               articles.map((article: ArticleObj) => {
